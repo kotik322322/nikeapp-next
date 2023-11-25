799303d188dcb1c54a38d8104db87ca5d315ab9b
kotik322322 <kotan23111995@gmail.com>
2023-11-25 02:04:31 +0200

    uncomited button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,14 +12,14 @@ export default function Home() {
       >
         <source src="/videos/videobg.mp4" type="video/mp4" />
       </video>
-      {/* <div className=" text-center absolute bottom-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full">
+      <div className=" text-center absolute bottom-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-full">
         <Link
           href={"/products/all"}
           className="px-10 py-4 md:px-20 md:hover:px-40 items-center justify-center rounded-full border  border-white text-white  bg-black bg-opacity-20  hover:bg-opacity-40  hoverEffect"
         >
           Shop Now
         </Link>
-      </div> */}
+      </div>
     </div>
   );
 }
